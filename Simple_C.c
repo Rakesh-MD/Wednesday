4358960b930e336c5ff47abe6d24c43282387cc1
Rakesh-MD <rakeshdadde@gmail.com>
2023-08-10 16:45:50 +0530

    first commit

--- Simple_C.c
+++ Simple_C.c
@@ -4,7 +4,7 @@ int main()
 {
    
      printf(" Hello Rakesh Welcome to GitHub Chnged in Server Side \n");
-     printf("\nLON020\n.............. Now Its 0002 .............\n ");
+     printf("\nLON020\n.............. Now Its 0003 .............\n ");
 
    printf(" Hello Rakesh Welcome to GitHub\n");
     show();
