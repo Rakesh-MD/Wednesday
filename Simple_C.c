e4e1e311c8fd531428d43dd53b238a2b6b4dc232
Rakesh-MD <141899837+Rakesh-MD@users.noreply.github.com>
2023-08-10 16:57:44 +0530

    12

--- Simple_C.c
+++ Simple_C.c
@@ -4,7 +4,7 @@ int main()
 {
    
      printf(" Hello Rakesh Welcome to GitHub Chnged in Server Side \n");
-     printf("\nLON020\n.............. Now Its 0011 .............\n ");
+     printf("\nLON020\n.............. Now Its 0012 .............\n ");
 
    printf(" Hello Rakesh Welcome to GitHub\n");
     show();
