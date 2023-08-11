1d10e93f89d631a74358ffcc5dca10b2babafff2
Rakesh-MD <rakeshdadde@gmail.com>
2023-08-11 10:30:37 +0530

    Cheking

--- Simple_C.c
+++ Simple_C.c
@@ -4,7 +4,7 @@ int main()
 {
    
      printf(" Hello Rakesh Welcome to GitHub  \n");
-     printf("\nLON020\n.............. Now Its 3rd .............\n\n ");
+     printf("\nLON020\n.............. Now Its 4th .............\n\n ");
        printf("\n\n.............. Now Its Server Side 2nd .............\n\n ");
     show();
 printf(" .............Good Bye.....\n");
