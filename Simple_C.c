3caddaaedc06eb162d01c017b955c4621108aa0e
Rakesh-MD <141899837+Rakesh-MD@users.noreply.github.com>
2023-08-11 15:12:11 +0530

    4th

--- Simple_C.c
+++ Simple_C.c
@@ -5,7 +5,7 @@ int main()
    
      printf(" Hello Rakesh Welcome to GitHub  \n");
      printf("\nLON020\n.............. Now Its 4th .............\n\n ");
-       printf("\n\n.............. Now Its Server Side 3rd Understand the fetch .............\n\n ");
+       printf("\n\n.............. Now Its Server Side 4th Understand the fetch .............\n\n ");
     show();
 printf(" .............Good Bye.....\n");
   
