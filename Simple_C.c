ad628ddb33d7716ccaf1dae567f199635c959e2b
Rakesh-MD <141899837+Rakesh-MD@users.noreply.github.com>
2023-08-11 09:54:15 +0530

    2nd Review

--- Simple_C.c
+++ Simple_C.c
@@ -5,7 +5,7 @@ int main()
    
      printf(" Hello Rakesh Welcome to GitHub  \n");
      printf("\nLON020\n.............. Now Its 2nd .............\n\n ");
-       printf("\n\n.............. Now Its Server Side 1st .............\n\n ");
+       printf("\n\n.............. Now Its Server Side 2nd .............\n\n ");
     show();
 printf(" .............Good Bye.....\n");
   
